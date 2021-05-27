44f7d2a76aa8a2c5d3b153606e80b0fe016b8505
jasper <jasper.chen@sain3.com>
2021-05-27 10:20:55 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## ClouSale\AmazonSellingPartnerAPI
+
+参考 https://github.com/clousale/amazon-sp-api-php
+
+使用 psr-4 方式加载，保留原有命名空间，方便后续转移或者添加新模块
+
+## 更新日志
+
+### 2021-05-12 
+
+添加 FeedsApi
+
---
+++ SellingPartnerOAuth.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace ClouSale\AmazonSellingPartnerAPI;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use GuzzleHttp\RequestOptions;
+
+class SellingPartnerOAuth
+{
+    /**
+     * @param $refreshToken
+     * @param $clientId
+     * @param $clientSecret
+     *
+     * @throws GuzzleException
+     */
+    public static function getAccessTokenFromRefreshToken($refreshToken, $clientId, $clientSecret)
+    {
+        $client = new Client();
+        $params = [
+            'grant_type' => 'refresh_token',
+            'refresh_token' => $refreshToken,
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+        ];
+
+        if ( ! defined('CURL_SSLVERSION_TLSv1_2')) {
+            define('CURL_SSLVERSION_TLSv1_2', 6);
+        }
+        
+        $options = array_merge([
+            RequestOptions::HEADERS => ['Accept' => 'application/json'],
+            RequestOptions::HTTP_ERRORS => false,
+            'curl' => [
+                CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
+            ],
+        ], $params ? [RequestOptions::FORM_PARAMS => $params] : []);
+
+        $response = $client->request('POST', 'https://api.amazon.com/auth/o2/token', $options);
+
+        $body = $response->getBody()->getContents();
+        $bodyAsJson = json_decode($body, true);
+
+        return $bodyAsJson['access_token'];
+    }
+
+    /**
+     * @throws GuzzleException
+     * @throws SellingPartnerOAuthException
+     */
+    public static function getRefreshTokenFromLwaAuthorizationCode(
+        string $lwaAuthorizationCode,
+        string $clientId,
+        string $clientSecret,
+        string $redirectUri
+    ) {
+        $client = new Client();
+        $params = [
+            'grant_type' => 'authorization_code',
+            'client_id' => $clientId,
+            'client_secret' => $clientSecret,
+            'code' => $lwaAuthorizationCode,
+            'redirect_uri' => $redirectUri,
+        ];
+        $options = array_merge([
+            RequestOptions::HEADERS => ['Accept' => 'application/json'],
+            RequestOptions::HTTP_ERRORS => false,
+            'curl' => [
+                CURLOPT_SSLVERSION => CURL_SSLVERSION_TLSv1_2,
+            ],
+        ], $params ? [RequestOptions::FORM_PARAMS => $params] : []);
+
+        $response = $client->request('POST', 'https://api.amazon.com/auth/o2/token', $options);
+
+        $body = $response->getBody()->getContents();
+        $bodyAsJson = json_decode($body, true);
+        if (isset($bodyAsJson['error_description'])) {
+            throw new SellingPartnerOAuthException($bodyAsJson['error_description'], $bodyAsJson['error']);
+        }
+
+        return $bodyAsJson['refresh_token'];
+    }
+}
